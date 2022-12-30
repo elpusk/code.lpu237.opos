ceefc51a7996d10f975d364383f30dbcba978e04
elpusk006 <ssyang@elpusk.co.kr>
2022-12-30 11:45:49 +0900

    coding opos manger

--- tp_opos_msr/tp_opos_msr.cpp
+++ tp_opos_msr/tp_opos_msr.cpp
@@ -11,21 +11,26 @@ int main()
 {
     std::wcout << L"Hello World!\n";
 
-    
-
     do {
-        _ns_tools::ct_opos_mgmt::type_ptr ptr_msr = _ns_tools::ct_opos_mgmt::get_manager(_ns_tools::ct_opos_mgmt::type_opos_msr);
-        if (!ptr_msr) {
+        _ns_tools::ct_opos_mgmt *p_msr = _ns_tools::ct_opos_mgmt::get_manager(_ns_tools::ct_opos_mgmt::type_opos_msr);
+        if (!p_msr) {
             std::wcout << L"ER : create mgmt msr.\n";
             continue;
         }
-        if (!ptr_msr->is_ini()) {
+        if (!p_msr->is_ini()) {
             std::wcout << L"ER : ini create mgmt msr.\n";
             continue;
         }
 
-        ptr_msr->wait_event();
+        for (int i = 0; i < 1; i++) {
+            std::wcout << L"R" << i+1 <<L": read.\n";
+            //p_msr->wait_event();
+        }
 
+        Sleep(1000);
+
+        
+        /*
         _ns_tools::ct_opos_mgmt::type_ptr ptr_keylock = _ns_tools::ct_opos_mgmt::get_manager(_ns_tools::ct_opos_mgmt::type_opos_keylock);
         if (!ptr_keylock) {
             std::wcout << L"ER : create mgmt keylock.\n";
@@ -35,6 +40,8 @@ int main()
             std::wcout << L"ER : ini create mgmt keylock.\n";
             continue;
         }
+        */
 
+        _ns_tools::ct_opos_mgmt::get_manager(_ns_tools::ct_opos_mgmt::type_opos_all, true);
     }while(false);
 }
